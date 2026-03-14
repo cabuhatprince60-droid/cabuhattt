0d4997678020573a2d33dcabc8e2311c1f0efda3
cabuhatprince60-droid <cabuhatprince60@gmail.com>
2026-03-14 18:47:18 +0800

    Add files via upload

---
+++ CABUHAT.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LastNametxt = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.FirstNametxt = new System.Windows.Forms.Label();
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbMonth = new System.Windows.Forms.ComboBox();
+            this.cmdDay = new System.Windows.Forms.ComboBox();
+            this.cmbYear = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(365, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "STUDENT REGISTRATION FORM";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LastNametxt
+            // 
+            this.LastNametxt.AutoSize = true;
+            this.LastNametxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LastNametxt.Location = new System.Drawing.Point(22, 76);
+            this.LastNametxt.Name = "LastNametxt";
+            this.LastNametxt.Size = new System.Drawing.Size(121, 25);
+            this.LastNametxt.TabIndex = 2;
+            this.LastNametxt.Text = "Last name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(17, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(243, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // FirstNametxt
+            // 
+            this.FirstNametxt.AutoSize = true;
+            this.FirstNametxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FirstNametxt.Location = new System.Drawing.Point(22, 126);
+            this.FirstNametxt.Name = "FirstNametxt";
+            this.FirstNametxt.Size = new System.Drawing.Size(116, 25);
+            this.FirstNametxt.TabIndex = 4;
+            this.FirstNametxt.Text = "first name";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 154);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(252, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Middle name";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 205);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(243, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 228);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Gender";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(98, 234);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(52, 17);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Male";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(156, 234);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(65, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Female";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(46, 265);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 24);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Date of birth";
+            // 
+            // cmbMonth
+            // 
+            this.cmbMonth.FormattingEnabled = true;
+            this.cmbMonth.Location = new System.Drawing.Point(12, 292);
+            this.cmbMonth.Name = "cmbMonth";
+            this.cmbMonth.Size = new System.Drawing.Size(121, 21);
+            this.cmbMonth.TabIndex = 12;
+            // 
+            // cmdDay
+            // 
+            this.cmdDay.FormattingEnabled = true;
+            this.cmdDay.Location = new System.Drawing.Point(156, 292);
+            this.cmdDay.Name = "cmdDay";
+            this.cmdDay.Size = new System.Drawing.Size(121, 21);
+            this.cmdDay.TabIndex = 13;
+            // 
+            // cmbYear
+            // 
+            this.cmbYear.FormattingEnabled = true;
+            this.cmbYear.Location = new System.Drawing.Point(305, 292);
+            this.cmbYear.Name = "cmbYear";
+            this.cmbYear.Size = new System.Drawing.Size(121, 21);
+            this.cmbYear.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(31, 316);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(152, 24);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Program to apply";
+            this.label7.Click += new System.EventHandler(this.label7_Click_1);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(17, 343);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(189, 21);
+            this.comboBox4.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(27, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 45);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Register student";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 490);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cmbYear);
+            this.Controls.Add(this.cmdDay);
+            this.Controls.Add(this.cmbMonth);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.FirstNametxt);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.LastNametxt);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LastNametxt;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label FirstNametxt;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbMonth;
+        private System.Windows.Forms.ComboBox cmdDay;
+        private System.Windows.Forms.ComboBox cmbYear;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CABUHAT.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+  using System;
+using System.Windows.Forms;
+
+namespace StudentRegistrationApplication // Matching namespace
+{
+    public partial class StudentRegistration : Form
+    {
+        public StudentRegistration()
+        {
+            object value = InitializeComponent();
+        }
+
+        private object InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void StudentRegistration_Load(object sender, EventArgs e)
+        {
+            // Populate Date of Birth ComboBoxes
+            cmbDay.Items.Add("-Day-");
+            cmbMonth.Items.Add("-Month-");
+            cmbYear.Items.Add("-Year-");
+
+            for (int i = 1; i <= 31; i++) cmbDay.Items.Add(i);
+            for (int i = 1; i <= 12; i++) cmbMonth.Items.Add(i);
+            for (int i = 1900; i <= DateTime.Now.Year; i++) cmbYear.Items.Add(i);
+
+            cmbDay.SelectedIndex = 0;
+            cmbMonth.SelectedIndex = 0;
+            cmbYear.SelectedIndex = 0;
+        }
+
+        private void Register_Click(object sender, EventArgs e)
+        {
+            // Validation Logic
+            if (string.IsNullOrWhiteSpace(LastNametxt.Text) || string.IsNullOrWhiteSpace(FirstNametxt.Text))
+            {
+                MessageBox.Show("Please fill out the required name fields.", "Validation Error");
+                return;
+            }
+
+            string gender = rbMale.Checked ? "Male" : (rbFemale.Checked ? "Female" : "Not Selected");
+            string studentInfo = $"Name: {FirstNametxt.Text} {LastNametxt.Text} {MiddleNametxt.Text}\nGender: {gender}";
+
+            MessageBox.Show(studentInfo, "Registration Successful");
+        }
+    }
